09dd4ba45063469facfec13294f46aca17ed61e1
pooooh14 <monisha1497@gmail.com>
2023-02-26 22:10:52 +0530

    some changes

---
+++ src/scripts/home.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 6,
+    spaceBetween: 30,
+    loop: true,
+    keyboard: {
+      enabled: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
